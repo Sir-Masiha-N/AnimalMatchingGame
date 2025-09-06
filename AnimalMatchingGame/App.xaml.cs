a582aa16744aa5151a6a91e0e43714792357f755
Sir-Masiha-N <masihanomi@gmail.com>
2025-09-06 13:26:27 +0330

    Add project files.

---
+++ AnimalMatchingGame/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = new Window(new AppShell());
+            const int newWidth = 500;
+            const int newHeight = 700;
+            window.X = 500;
+            window.Y = 80;
+            window.Width = newWidth;
+            window.Height = newHeight;
+            return window;
+        }
+    }
+}
